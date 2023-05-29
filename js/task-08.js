bb11faa36c4a1b233dcb175849c8e9a068482362
Xertaron <xertaron@gmail.com>
2023-05-29 23:49:25 +0200

    tuż tuż

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const login = document.querySelector("form.login-form");
+const email = login.email;
+const password = login.password;
+const button = document.querySelector("button");
 
+login.addEventListener("submit", (event) => {
+  event.preventDefault();
+  if (email.value.length === 0 || password.value.length === 0) {
+    alert("Uzupełnij pola!");
+  } else {
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+    console.log(`Email ${email.value} Password ${password.value}`);
+    login.reset();
+  }
+});
